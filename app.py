9267ca0d2aeb4a8b0b9dececb2a1e8e585f50993
yelimk01 <yelimk01@naver.com>
2024-11-14 07:20:29 +0900

    deepLearning 연동

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from datetime import datetime  # datetime 모듈 import 추가
 from pytz import timezone
 from fastapi.responses import HTMLResponse
 
+from deepLearning import *
 
 app = FastAPI()
 
@@ -21,6 +22,7 @@ if not os.path.exists(SAVE_DIR):
 @app.get("/")
 def read_root():
     return {"message": "root test success!"}
+
 
 # HTML 파일을 반환하는 루트 경로 응답
 @app.get("/", response_class=HTMLResponse)
@@ -47,9 +49,6 @@ async def receive_gps_data(data: GPSData):
         print(
             f"Received GPS Data - Latitude: {data.latitude}, Longitude: {data.longitude}, Altitude: {data.altitude}, Heading: {data.heading}")
 
-        #좌표 계산(객체 좌표로 수정해야 함)
-        CalculateCoordinate(5000, 300, data)
-
         # Base64 이미지 데이터를 파일로 저장
         image_data = base64.b64decode(data.image)
         timestamp = datetime.now(timezone('Asia/Seoul')).strftime("%Y%m%d_%H%M%S")
@@ -57,6 +56,14 @@ async def receive_gps_data(data: GPSData):
 
         with open(image_filename, "wb") as img_file:
             img_file.write(image_data)
+
+            # 딥러닝 후 경계상자가 표시된 이미지(yolo_image_base64), 경계상자의 중앙좌표 반환(center_coordinates)
+            yolo_image_base64, center_coordinates = detect_and_draw_boxes(image_data)
+
+            # 좌표 계산(객체 좌표로 수정해야 함)
+            # CalculateCoordinate(5000, 300, data)
+            # 경계상자의 갯수 = len(center_coordinates)
+            CalculateCoordinate(center_coordinates[0]["center_x"], center_coordinates[0]["center_y"])
 
         print(f"Image saved as {image_filename}")
 
