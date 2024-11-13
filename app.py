1e6b289dc26b78804da0805b7bc891f8710e4c21
yelimk01 <yelimk01@naver.com>
2024-11-13 14:06:51 +0900

    coordinate calculation 연동

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from datetime import datetime  # datetime 모듈 import 추가
 from pytz import timezone
+from fastapi.responses import HTMLResponse
+
 
 app = FastAPI()
 
@@ -14,10 +16,19 @@ SAVE_DIR = "received_images"  # SAVE_DIR 경로 설정
 if not os.path.exists(SAVE_DIR):
     os.makedirs(SAVE_DIR)  # 디렉토리 생성
 
+
 # 루트 경로 응답 (테스트용)
 @app.get("/")
 def read_root():
     return {"message": "root test success!"}
+
+# HTML 파일을 반환하는 루트 경로 응답
+@app.get("/", response_class=HTMLResponse)
+async def read_index():
+    with open("map/map.html", "r", encoding="utf-8") as file:
+        html_content = file.read()
+    return HTMLResponse(content=html_content)
+
 
 # GPS 및 이미지 데이터를 받을 모델 정의
 class GPSData(BaseModel):
@@ -27,12 +38,17 @@ class GPSData(BaseModel):
     heading: float
     image: str  # Base64 인코딩된 이미지 데이터
 
+from coordinate import *
 # /gps-data 엔드포인트로 POST 요청을 받는 API 생성
 @app.post("/gps-data")
 async def receive_gps_data(data: GPSData):
     try:
         # 받은 GPS 데이터만 로그 출력
-        print(f"Received GPS Data - Latitude: {data.latitude}, Longitude: {data.longitude}, Altitude: {data.altitude}, Heading: {data.heading}")
+        print(
+            f"Received GPS Data - Latitude: {data.latitude}, Longitude: {data.longitude}, Altitude: {data.altitude}, Heading: {data.heading}")
+
+        #좌표 계산(객체 좌표로 수정해야 함)
+        CalculateCoordinate(5000, 300, data)
 
         # Base64 이미지 데이터를 파일로 저장
         image_data = base64.b64decode(data.image)
@@ -43,13 +59,14 @@ async def receive_gps_data(data: GPSData):
             img_file.write(image_data)
 
         print(f"Image saved as {image_filename}")
-        
+
         return {"status": "success", "message": "Data and image received successfully"}
 
     except Exception as e:
         # 예외가 발생하면 오류 메시지 출력
         print(f"Error: {e}")
         raise HTTPException(status_code=500, detail="An error occurred while processing the data.")
+
 
 # 이 파일이 직접 실행될 때 main() 함수 호출
 if __name__ == "__main__":
